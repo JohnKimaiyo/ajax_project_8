95e080839ac40f9fbb812f9c20acb829bf4482c5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-09-30 09:33:05 +0300

    ading api data logic

--- index.js
+++ index.js
@@ -23,3 +23,61 @@ let displayTextData = (data) => {
     let htmlTemplate = `<h3>${data}</h3>`;
     document.querrySelector('#text-card').innerHTML = htmlTemplate;
 }
+
+// json button
+let jsonButton = document.querySelector("json-btn");
+jsonButton.addEventListener('click', function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', './mobile.json', true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = xhr.reposnseText;
+            let mobile = Json.parser(data);
+            console.log(mobile);
+        }
+    }
+});
+
+// display json data
+let displayJSONData = (mobile) => {
+    let htmlTemplate = '';
+    htmlTemplate = `<ul>
+    <li>ID :${mobile.id}</li>
+    <li>BRAND:${mobile.brand}</li>
+    <li>BRAND:${mobile.color}</li>
+    <li>BRAND:${mobile.price}</li>
+</ul>
+    `
+    document.querySelector('#json-card').innerHTML = htmlTemplate
+};
+
+// api button
+let apiButton = document.querySelector('#api-btn');
+apiButton.addEventListener('click', function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = xhr.reposnseText;
+            let users = JSON.parse(data);
+            console.log(users);
+        }
+    }
+})
+
+// dipslay users
+let displayUsers = (users) => {
+    letTemplate = '';
+    for (let user of users) {
+        htmlTemplate += `<ul class = "list-group">
+        <li class = "list-group-item"> ID:${user.id}</li>
+        <li class = "list-group-item"> ID:${user.name}</li>
+        <li class = "list-group-item"> ID:${user.email}</li>
+        <li class = "list-group-item"> ID:${user.address.street}</li>
+        <li class = "list-group-item"> ID:${user.address.city}</li>
+        `
+    }
+    document.querySelector('#api-card').innerHTML = htmlTemplate;
+}
